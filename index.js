d9be3771578a321879edc8b8778c7b832108ad56
Simon Taylor <nizmox@gmail.com>
2017-05-18 09:25:59 +1000

    initial code

---
+++ index.js
@@ -0,0 +1,12 @@
+const split = (array, fn) => {
+  return array.reduce((result, value) => {
+    const key = fn(value);
+    // discard values which resolve to a null or undefined key
+    if (key == null) return result;
+    if (result[key] == null) result[key] = [value];
+    else result[key].push(value);
+    return result;
+  }, {})
+}
+
+module.exports = split;
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,56 @@
 const test = require('ava');
+const split = require('../index');
 
-test('description here', (t) => {
-  // do a thing
+test('it must return an object', (t) => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const result = split(arr, (i) => {
+    if (i >= 5) return '5+';
+    if (i === 1) return '1';
+    return 'other';
+  });
+
+  t.is(typeof result, 'object');
+});
+
+test('it must return expected keys count', (t) => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const result = split(arr, (i) => {
+    if (i >= 5) return '5+';
+    if (i === 1) return '1';
+    return 'other';
+  });
+
+  t.is(Object.keys(result).length, 3);
+});
+
+test('it must drop null values', (t) => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const result = split(arr, (i) => {
+    if (i >= 5) return '5+';
+    if (i === 1) return '1';
+    return null;
+  });
+
+  const expected = {
+    '1': [1],
+    '5+': [5, 6, 7, 8]
+  };
+
+  t.deepEqual(result, expected);
+});
+
+
+test('it must drop undefined values', (t) => {
+  
+});
+
+test('it must stringify numbers', (t) => {
+  
+});
+
+test('it must not include keys with no value', (t) => {
+  
 });
